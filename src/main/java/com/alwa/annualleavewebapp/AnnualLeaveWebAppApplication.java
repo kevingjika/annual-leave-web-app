afba17c8842ac86476f7a664f9a61a5363e0320d
kevin.gjika <gjikakevin@gmail.com>
2022-09-08 12:03:30 +0200

    mesazhi

---
+++ src/main/java/com/alwa/annualleavewebapp/AnnualLeaveWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.alwa.annualleavewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnualLeaveWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnualLeaveWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alwa/annualleavewebapp/AnnualLeaveWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwa.annualleavewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnualLeaveWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
