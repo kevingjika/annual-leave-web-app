e7ed4af1409c498feaa46133f1478404d39cded1
kevin.gjika <gjikakevin@gmail.com>
2022-09-09 15:30:17 +0200

    mesazhi

---
+++ src/main/java/com/alwa/annualleavewebapp/controllers/FormController.java
@@ -0,0 +1,29 @@
+package com.alwa.annualleavewebapp.controllers;
+
+import com.alwa.annualleavewebapp.entities.UserForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class FormController {
+
+@GetMapping("register")
+    public String userRegistrationPage (Model model) {
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
+        return "register-form";
+    }
+
+    @PostMapping("register/save")
+    public String submitForm(Model model, @ModelAttribute("userForm") UserForm userForm) {
+        model.addAttribute("userForm", userForm);
+        return "register-success";
+    }
+
+}
---
+++ src/main/java/com/alwa/annualleavewebapp/entities/UserForm.java
@@ -0,0 +1,61 @@
+package com.alwa.annualleavewebapp.entities;
+
+public class UserForm {
+
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
